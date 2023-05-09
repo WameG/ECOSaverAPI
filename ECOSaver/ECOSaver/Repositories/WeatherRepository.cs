5f651b972140c2414af85f2dd1304413209ae8f6
WameG <wgassama@yahoo.dk>
2023-05-09 10:09:25 +0200

    Fix

--- ECOSaver/ECOSaver/Repositories/WeatherRepository.cs
+++ ECOSaver/ECOSaver/Repositories/WeatherRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace ECOSaver.Repositories
+﻿using ECOSaver.Models;
+
+namespace ECOSaver.Repositories
 {
     public class WeatherRepository
     {
+        private List<Weather> _weathers;
+
+        public WeatherRepository()
+        {
+            _weathers = new List<Weather>();
+        }
+
+        public List<Weather> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Weather Add(Weather newWeather) {
+            throw new NotImplementedException();
+        }
+
+        public Weather Delete(int id) { 
+            throw new NotImplementedException();
+        }
     }
 }
