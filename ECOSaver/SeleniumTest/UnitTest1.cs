bd50c4743eb45b751c511aa47745416aaf534a30
Haydar02 <hayd0281@edu.easj.dk>
2023-05-16 22:43:20 +0200

    udvidet Selenuim Test

--- ECOSaver/SeleniumTest/UnitTest1.cs
+++ ECOSaver/SeleniumTest/UnitTest1.cs
@@ -28,7 +28,7 @@ namespace SeleniumTest
             string url = "https://ecosaver.azurewebsites.net/";
             _driver.Navigate().GoToUrl(url);
             string title = _driver.Title;
-            Assert.AreEqual(title,_driver.Title);
+            Assert.AreEqual(title, _driver.Title);
             IWebElement SelectCity = _driver.FindElement(By.CssSelector("select.form-select"));
             SelectElement select = new SelectElement(SelectCity);
             select.SelectByText("Copenhagen");
@@ -54,5 +54,47 @@ namespace SeleniumTest
             Assert.IsTrue(temperatureText.Text.Contains("Temperatur"));
             Assert.IsTrue(descriptionText.Text.Contains("Beskrivelse"));
         }
+        [TestMethod]
+        public void TestPageElements()
+        {
+
+            _driver.Navigate().GoToUrl("https://ecosaver.azurewebsites.net/");
+
+
+            IWebElement energyCard = _driver.FindElement(By.CssSelector("div.card-body > h5.card-title"));
+            Assert.AreEqual("Vælg by for vejrudsigten*", energyCard.Text);
+           
+            IWebElement energyButton = _driver.FindElement(By.CssSelector("div.card-body > button.btn"));
+            Assert.AreEqual("Vis vejr", energyButton.Text);
+
+           
+            // Verify the "Vejr i {{locationCity}}" card
+            IWebElement weatherCard = _driver.FindElements(By.CssSelector("div.card-body > h5.card-title"))[1];
+            Assert.AreEqual("Vælg tidspunkt", weatherCard.Text);
+
+            IWebElement temperatureElement = _driver.FindElement(By.XPath("//p[contains(text(), 'Temperatur:')]"));
+            Assert.IsTrue(temperatureElement.Text.Contains("Temperatur:"));
+
+            IWebElement humidityElement = _driver.FindElement(By.XPath("//p[contains(text(), 'Fugtighed:')]"));
+            Assert.IsTrue(humidityElement.Text.Contains("Fugtighed:"));
+
+            IWebElement outdoorDryElement = _driver.FindElement(By.XPath("//p[contains(text(), 'Udendørs tøjophæng?')]"));
+            Assert.IsTrue(outdoorDryElement.Text.Contains("Udendørs tøjophæng?"));
+
+            IWebElement weatherButton = _driver.FindElement(By.CssSelector("div.card-body > button.btn"));
+            Assert.AreEqual("Vis vejr", weatherButton.Text);
+                       
+
+            IWebElement powerHistoryCard = _driver.FindElements(By.CssSelector("div.card-body > h5.card-title"))[2];
+            Assert.AreEqual("Gennemsnit pris for i dag", powerHistoryCard.Text);
+
+
+            IWebElement averagePriceDay1 = _driver.FindElement(By.XPath("//p[contains(text(), 'Gennemsnit for 0 dage siden:')]"));
+            Assert.IsTrue(averagePriceDay1.Text.Contains("Gennemsnit for 0 dage siden:"));
+
+            IWebElement averagePriceDay2 = _driver.FindElement(By.XPath("//p[contains(text(), 'Gennemsnit for 1 dage siden:')]"));
+            Assert.IsTrue(averagePriceDay2.Text.Contains("Gennemsnit for 1 dage siden:"));
+        }
+
     }
 }
